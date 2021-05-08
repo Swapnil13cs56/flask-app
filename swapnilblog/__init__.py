1705fdda765ac97da637de5d183c191d1de0a613
swapnil13cs56 <swapnil13cs56@gmail.com>
2021-05-08 21:43:24 +0530

    login registration flow

--- swapnilblog/__init__.py
+++ swapnilblog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -7,7 +9,10 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'c8324c68a2be34541966a70fcd4321a6'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from swapnilblog import routes
